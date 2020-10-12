cd67c8a48c760081f7b5f8b34f75db82625832bc
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2020-10-12 19:47:25 +0100

    finished styling components

---
+++ src/components/menu-item-component/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './menu-item.styles.scss'
+
+export const MenuItem = ({title, imageUrl, size, linkUrl, match, history}) => (
+    <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div className='background-image' style={{backgroundImage: `url("${imageUrl}")`}} />
+        <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+            <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+)
